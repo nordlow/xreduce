bbc055b09e43cad57907cc54ef4c04d9d9fe4171
Per Nordlöw <per.nordlow@gmail.com>
2024-10-05 10:38:14 +0200

    Update outputs.sh

--- outputs.sh
+++ outputs.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Check if correct number of arguments is provided
 if [ "$#" -lt 2 ]; then
-	echo "Usage: $0 OUTPUT CMD [ARG...]
+	echo "Usage: $0 STDOUT_OR_STDERR_OUTPUT CMD [ARG...]
 
 To few arguments given."
   exit 1
 fi
 
-# Store the first argument as the expected output
 EXPECTED_OUTPUT="$1"
 shift # Shift all arguments left, so CMD becomes the new first argument
 
-# Run CMD and capture its stdout and stderr
+# Run CMD and capture its stdout and stderr into OUTPUT
 OUTPUT=$( "$@" 2>&1 )
 
 # Check if OUTPUT contains the expected output string
